c671f45527475ad02e157e59ce3a4d0218a994f4
Victor Sarrís Silva Santos <devvictorsarris@gmail.com>
2025-07-21 16:19:09 -0300

    reactions

--- projeto1/src/App.jsx
+++ projeto1/src/App.jsx
@@ -3,10 +3,10 @@ import Titulo from './Titulo.jsx'
 function App(){
   return (
     <div>
-      <Titulo cor ="red" nome="Dimitri" paragrafo={true} />
-      <Titulo cor = "blue" nome="Victor Sarrís 🪽" paragrafo={true}/>
-      <Titulo cor= "purple" paragrafo={false}/>
-      <Titulo paragrafo={true}/>
+      <Titulo cor ="red"/>
+      <Titulo cor = "blue"/>
+      <Titulo cor= "purple"/>
+      <Titulo cor="orangered"/>
     </div>
   )
 }
--- projeto1/src/Titulo.jsx
+++ projeto1/src/Titulo.jsx
@@ -1,14 +1,19 @@
-function Titulo({nome, paragrafo, cor}) {
-    const urlImg = "https://imgs.search.brave.com/QdvECf7Yi_8F22Au4sW37pCx60_frJZ0WgqYy1hN9qk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdG9y/YWdlLmdvb2dsZWFw/aXMuY29tL3N0YXIt/bGFiL2Jsb2cvT0dz/L3JlYWN0LnBuZw";
+import { useState } from "react";
+
+function Titulo({cor}) {
+
+    const [texto, setTexto] = useState("Um titulo do estado inicial")
+    const [inputText, setInputText] = useState("")
+
+    function clicou(){
+        setTexto(inputText);
+    }
+
     return (
         <div>
-            <h1 style={{color: cor }}>Oi eu sou um título {nome ? nome: "Fulano"}</h1>
-            {paragrafo ? 
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus ipsum assumenda aspernatur animi aut debitis, perspiciatis suscipit mollitia magni commodi ab fuga similique maiores delectus culpa porro, obcaecati recusandae rem!</p>
-                :
-                <p>Nada</p>
-            }
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum ratione, corrupti magnam ullam illum aut unde cumque ipsam tenetur nisi velit eius sit neque! Sapiente saepe optio inventore aperiam? Totam.</p>
+            <h1 style={{color: cor }}>{texto}</h1>
+            <input type="text" value={inputText} onChange={(e)=>{setInputText(e.target.value)}}/>
+            <button onClick={clicou}>Mudar</button>
         </div>
     )
 }
